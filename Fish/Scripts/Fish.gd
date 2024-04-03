6bdbf1757b3595c9eaa60343f104672aa4a60825
Adam <36433367+tarinyoom@users.noreply.github.com>
2024-04-02 20:17:24 -0700

    add propulsion (#1)
    adds propulsion and some walls

--- Fish/Scripts/Fish.gd
+++ Fish/Scripts/Fish.gd
@@ -1,9 +1,19 @@
 extends RigidBody3D
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("click"):
-		apply_force_direction(self, Vector3(0,1,0), 10.0) # Adjust strength as needed
+var camera
 
+func _ready():
+	camera = get_viewport().get_camera_3d()
+	
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		var unscaled = self.transform.origin - camera.project_position(event.position, camera.transform.origin.z)
+		var scaled = unscaled.normalized()
+		apply_force_direction(self, scaled, 10.0) # Adjust strength as needed
+
+func _process(delta) -> void:
+	camera.transform.origin.x = self.transform.origin.x
+	camera.transform.origin.y = self.transform.origin.y
 
 func apply_force_direction(rigid_body: RigidBody3D, direction: Vector3, strength: float) -> void:
 	var force: Vector3 = direction.normalized() * strength
