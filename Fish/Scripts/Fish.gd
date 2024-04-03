e2fa23490c4728f534f3e451a443704fa063644a
Adam <adam@tarinyoom.io>
2024-04-02 17:57:11 -0700

    updates fish script to listen for input events

--- Fish/Scripts/Fish.gd
+++ Fish/Scripts/Fish.gd
@@ -1,9 +1,9 @@
 extends RigidBody3D
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("click"):
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton and event.is_pressed():
 		apply_force_direction(self, Vector3(0,1,0), 10.0) # Adjust strength as needed
-
 
 func apply_force_direction(rigid_body: RigidBody3D, direction: Vector3, strength: float) -> void:
 	var force: Vector3 = direction.normalized() * strength
